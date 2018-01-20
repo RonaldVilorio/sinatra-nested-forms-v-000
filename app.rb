13e78c303ebe3c23dd43dcdb7626bf55d8bc9cc9
ronald vilorio <soccer23415@gmail.com>
2018-01-20 19:09:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,13 +11,14 @@ module FormsLab
       erb :'/pirates/new'
     end
     post '/pirates' do
+      binding.pry
       @pirate = Pirate.new(params[:pirate][:name],params[:pirate][:weight],params[:pirate][:height])
 
       params[:pirate][:ships].each do |details|
         Ship.new(details[:name],details[:type],details[:booty])
       end
       @ships = Ship.all
-      
+
 
 
       erb :'/pirates/show'
